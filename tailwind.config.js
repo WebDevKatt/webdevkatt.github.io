c129a9909a0137da820b2d71a04e923d7cc16d3d
WebDevKatt <160454425+WebDevKatt@users.noreply.github.com>
2024-02-18 23:48:50 -0700

    Installs

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:/Users/Katt/Desktop/WebDevKatt.github.io.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
